f950e68fe48ad030a74c0d1671f87061db8d57d1
Javi25214 <javitoyrosita@gmail.com>
2023-07-31 10:39:15 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,33 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState, useEffect} from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	const [color, setColor] = useState('red');
+
+	useEffect(() => {
+		const cambiarColorAutomaticamente = () => {
+		  if (color === 'red') {
+			setColor('green');
+		  } else if (color === 'green') {
+			setColor('yellow');
+		  } else if (color === 'yellow') {
+			setColor('red');
+		  }
+		};
+		const intervalo = setInterval(cambiarColorAutomaticamente, 2000); // Cambiara cada 2 segundos
+
+		return () => {
+			clearInterval(intervalo);
+		  };
+		}, [color]);
+
+		return (
+			<div className="semaforo">
+			  <div className={`light red ${color === 'red' ? 'active' : ''}`}></div>
+			  <div className={`light yellow ${color === 'yellow' ? 'active' : ''}`}></div>
+			  <div className={`light green ${color === 'green' ? 'active' : ''}`}></div>
+			</div>
+		  );
+		};
 
 export default Home;
